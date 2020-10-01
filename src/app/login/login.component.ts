7f5beb4d52087f727932e728f8e0cd7d375ecd19
Rohit Bhatia <ROHIT.BHATIA@VIZIENTINC.COM>
2020-10-01 09:55:06 -0500

    pending commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,11 @@ import {LoginService} from '../service/login.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {OnDestroyMixin, untilComponentDestroyed} from '@w11k/ngx-componentdestroyed';
 
+class Organization {
+  orgKey: number;
+  orgValue: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,6 +20,14 @@ export class LoginComponent extends OnDestroyMixin implements OnInit {
 
   loginForm: FormGroup;
   submitted = false;
+  selectedOrg = 3;
+  organizationMetadata: Organization[] = [
+    {orgKey: 1, orgValue: 'org 1'},
+    {orgKey: 2, orgValue: 'org 2'},
+    {orgKey: 3, orgValue: 'org 3'},
+    {orgKey: 4, orgValue: 'org 4'},
+    {orgKey: 5, orgValue: 'org 5'},
+  ];
 
   constructor(public loginService: LoginService, private formBuilder: FormBuilder) {
     super();
@@ -36,6 +49,9 @@ export class LoginComponent extends OnDestroyMixin implements OnInit {
         Validators.required,
         Validators.minLength(3),
         Validators.pattern('[ -~]*')])],
+      organization: ['', Validators.compose(([
+        Validators.required,
+      ]))]
     });
     console.log(this.requiredErrorMessage);
   }
@@ -50,5 +66,9 @@ export class LoginComponent extends OnDestroyMixin implements OnInit {
       .subscribe(token => {
         this.loginService.saveToken(token.jwt);
       });
+  }
+
+  onOrganizationChange(target: any) {
+    console.log(target.value);
   }
 }
